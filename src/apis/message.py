837d4e9901f11dd71a2a1c5904ddb7648d5b99a0
Eric07110904 <azsx26735546@gmail.com>
2020-10-24 01:37:11 +0800

    add SMS msg function

---
+++ src/apis/message.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import urllib,os,requests,json
+
+load_dotenv()
+username = os.getenv("SMS_USER")
+password = os.getenv("SMS_PASS")
+
+def SendSMS(mobile,message):
+    global username
+    global password 
+    msg = 'username='+username+'&password='+password+'&mobile='+mobile+'&message='+urllib.parse.quote(message)
+    ans = requests.post('http://api.twsms.com/json/sms_send.php?'+msg)
+    return json.loads(ans.text)
+    #code 00020 點數不足
+    #code 00000 完成
+    #code 00030 IP無使用權限
+
